b8dd72f29ca8965fd551bc474508f4289e839ae0
Pablo <pablo.arenas@gracia.lasalle.cat>
2025-03-31 10:32:03 +0200

    Creacion del main, falta ahora desenvoluparlo

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
 package src;
+import java.util.Scanner;
+import src.controller.GestorTareas;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        GestorTareas gestor = new GestorTareas();
+        int opcion;
+    }
 }
